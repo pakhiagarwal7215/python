e5b825911fcaf133b7a172ec15dc78c486de36d5
pakhiagarwal7215 <garg.pankaj86@gmail.com>
2026-02-12 16:56:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import requests
-import random
-EDUCATION_CATEGORY_ID = 9
-API_URL = f"https://opentdb.com/api.php?amount=10&category={EDUCATION_CATEGORY_ID}&type=multiple"
-options = []
-response = requests.get(API_URL)
+
+# Example of facing a random joke
+
+url = "https://official-joke-api.appspot.com/random_joke"
+
+# Send GET request to fetch a joke
+
+response = requests.get(url)
+
 if response.status_code == 200:
-    data = response.json()
-    for i, qdata in enumerate(data["results"]):
-        print("question", i+1)
-        print(qdata["question"])
-        correct=qdata["correct_answer"]
-        options = qdata["incorrect_answers"]+[qdata["correct_answer"]]
-        options = sorted(options)
-        for j , opt in enumerate(options):
-            print(j+1, opt) 
-        choice = int(input("enter your answer 1, 2, 3 or 4  "))
-        if  choice<1 or choice >4:
-            print("You have entered invalid choice")
-        else:
-            if options[choice-1] == correct:
-                print("Correct!")
-            else:
-                print("incorrect")
+
+    joke_data = response.json()
+
+    print(f"Joke : {joke_data['setup']} - {joke_data['punchline']}")
+
+else:
+    
+    print(f"Failed to retrive joke.Status code: {response.status_code}")
