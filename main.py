c2b9603841f429442f7ec0807bb7ebde1927ee9b
pakhiagarwal7215 <garg.pankaj86@gmail.com>
2026-02-13 13:42:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import requests
 
-# Example of facing a random joke
-
-url = "https://official-joke-api.appspot.com/random_joke"
-
-# Send GET request to fetch a joke
-
-response = requests.get(url)
-
-if response.status_code == 200:
-
-    joke_data = response.json()
-
-    print(f"Joke : {joke_data['setup']} - {joke_data['punchline']}")
-
-else:
-    
-    print(f"Failed to retrive joke.Status code: {response.status_code}")
+URL = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+def generate_facts():
+    while True:
+        choice = input("press g to generate facts and press q to quit")
+        if choice == 'g':
+            response = requests.get(URL)
+            if response.status_code == 200:
+                data = response.json()
+                print(data['text'])
+        else:
+            print("Goodbye")
+if __name__ == "__main__":
+    generate_facts()   
