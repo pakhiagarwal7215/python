f70c62086a2c0006b375e24f6f7885fd40075c94
pakhiagarwal7215 <garg.pankaj86@gmail.com>
2026-05-18 12:09:39 +0530

    Add files via upload

---
+++ pakhiisofficialygivingup/main.py
@@ -0,0 +1,22 @@
+import cv2
+face_cascade =  cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+if cap.isOpened():
+    print("Camera activated")
+else:
+    print("Failed to capture the frame")
+    exit()
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        print("Failed to capture the frame")
+        break
+    gray_image = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_image,scaleFactor=1.1,minNeighbors=5,minSize=(30,30))
+    for x,y,w,h in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imshow('Face Detection - Press q to Quit', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
